a3c7e6cff95edeb2ad5ef1765c339eb3336d91a0
Emilio Grande <erixcbr@gmail.com>
2015-05-05 19:28:48 +0200

    buttons and colors

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+function heroColor(d, index) {
+  if (index === 0) { // ironman
+    return '#DA0716';
+  } else if (index === 1) {
+    return '#007315'; // hulk!
+  } else {
+    return '#006eff';
+  }
+}
+
+function addTen(heroBar) {
+  var oldVal = heroBar.getAttribute('width');
+  heroBar.setAttribute('width', parseInt(oldVal) + 10);
+}
+
 function init() {
 
   var data = [50, 75, 12];
@@ -13,7 +28,13 @@ function init() {
      .append('rect')
      .attr('height', 50)
      .attr('width', function(d){ return d;})
-     .attr('y', function(d, index){ return index * 55; });
+     .attr('y', function(d, index){ return index * 55; })
+     .attr('fill', heroColor);
+
+  document.getElementById('ironman').addEventListener('click', function() { addTen(heroBars[0][0]); });
+  document.getElementById('hulk').addEventListener('click', function() { addTen(heroBars[0][1]); });
+  document.getElementById('thor').addEventListener('click', function() { addTen(heroBars[0][2]); });
+
 }
 
 window.onload = init;
